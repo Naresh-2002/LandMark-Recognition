c97ddb9441a98e53b9cef9fe0a00b24a80a42c22
Naresh <105240762+Naresh-2002@users.noreply.github.com>
2023-04-30 16:45:14 +0530

    Update LM_Detection.py

--- LM_Detection.py
+++ LM_Detection.py
@@ -38,7 +38,7 @@ def run():
     st.image(img)
     img_file = st.file_uploader("Choose your Image", type=['png', 'jpg'])
     if img_file is not None:
-        save_image_path = 'Uploaded_Images/' + img_file.name
+        save_image_path = './Uploaded_Images/' + img_file.name
         with open(save_image_path, "wb") as f:
             f.write(img_file.getbuffer())
         prediction,image = image_processing(save_image_path)
